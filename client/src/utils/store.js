f8efe3c037d45093e733df076ac52a53e700b23a
Victoria Landron <viclandron@gmail.com>
2021-01-24 11:34:28 -0500

    fix to package-lock.json

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,10 +1,6 @@
 import { createStore } from 'redux';
 import reducer from './reducers';
 
-<<<<<<< HEAD
-export default createStore(reducer);
-=======
 const store = createStore(reducer)
 
 export default store;
->>>>>>> develop
